22a2eab2e2ffa6da596c153d8df77530385fc61a
aeyll-11 <taguine.idriss@gmail.com>
2023-02-15 01:25:29 +0100

    fix css

--- src/components/layout/topBar.tsx
+++ src/components/layout/topBar.tsx
@@ -52,9 +52,9 @@ export const TopBar = () => {
                     {navigatorLanguage}
                 </span>
             </div>
-            <div className="h-[0.8rem] mx-1 w-6 border border-white border-opacity-50 p-[1px] rounded-[4px]">
+            <div className="h-[0.8rem] mx-1 w-6 border border-white border-opacity-50 p-[1px] rounded-[3px]">
                 <div style={{width: batteryLevel + '%'}} className='relative h-full bg-white bg-opacity-90 rounded-[3px]'>
-                    <div className="absolute bg-white bg-opacity-50 h-[4px] w-[2.8px] rounded-r-full right-[-21px] top-[2.5px]"></div>
+                    <div className="absolute bg-opacity-50 bg-white h-[4px] w-[1.8px] rounded-r-md right-[-5px] top-[2.5px]"></div>
                 </div>
             </div>
             <span className="m-2">{date}</span>
